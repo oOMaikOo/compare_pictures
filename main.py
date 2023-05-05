5fd23b9cb4d82bfb4ed41b9f66c7a74be329bea1
maik <maik.loehn@gmx.de>
2023-05-05 07:45:35 +0200

    Update

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import sqlite3
 SUPPORTED_FORMATS = ['.jpg', '.jpeg', '.png', '.gif']
 
 # Der Pfad zum Ordner, den du durchsuchen möchtest
-DIRECTORY_TO_SEARCH = r'folder'
+DIRECTORY_TO_SEARCH = r'D:\#BACKUP\BILDER\4.Bilder_Neu_N900'
 
 # Der Pfad zur Datenbank
 DATABASE_PATH = 'picture.sqlite'
+
 
 # Eine Funktion, die die Tabelle images erstellt
 def create_table():
@@ -22,11 +23,13 @@ def create_table():
         )''')
         conn.commit()
 
+
 # Eine Funktion, die den Hashwert eines Bildes berechnet
 def calculate_hash(file_path):
     with open(file_path, 'rb') as f:
         file_bytes = f.read()
         return hashlib.sha256(file_bytes).hexdigest()
+
 
 # Eine Funktion, die ein Bild in die Datenbank einfügt
 def insert_into_database(file_path, hash_value):
@@ -34,6 +37,7 @@ def insert_into_database(file_path, hash_value):
         cursor = conn.cursor()
         cursor.execute('INSERT INTO images VALUES (?, ?, ?)', (file_path, os.path.dirname(file_path), hash_value))
         conn.commit()
+
 
 # Eine Funktion, die alle Bilder im Verzeichnis durchsucht und die gefundenen Bilder in die Datenbank einfügt
 def search_directory(directory):
@@ -44,6 +48,19 @@ def search_directory(directory):
                 hash_value = calculate_hash(file_path)
                 insert_into_database(file_path, hash_value)
 
+
+def find_duplicates():
+    with sqlite3.connect(DATABASE_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            'SELECT hash_value, GROUP_CONCAT(file_path, ", ") AS file_paths, directory_path FROM images GROUP BY hash_value HAVING COUNT(*) > 1')
+        duplicates = cursor.fetchall()
+        for hash_value, file_paths, directory_path in duplicates:
+            print(
+                f'Der Hashwert {hash_value} wurde in den folgenden Dateien gefunden: {file_paths} im Ordner {directory_path}')
+
+
 if __name__ == '__main__':
     create_table()
     search_directory(DIRECTORY_TO_SEARCH)
+    find_duplicates()
